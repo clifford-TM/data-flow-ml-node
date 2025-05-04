4096c13ffe9e54a2e219f32ce18927f0aab74c2b
clifford-TM <fdavid.hl3@gmail.com>
2025-05-04 18:21:17 -0300

    Adição da porta para o servidor azure

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.post('/auditoria', (req, res) => {
 });
 
 //Porta do servidor
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
